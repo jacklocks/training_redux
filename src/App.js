0341e25af0276c7c57a5b8b09342125fb187e316
jacklocks <castets.eric@free.fr>
2023-11-06 07:59:55 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PostForm from "./components/PostForm";
+import User from "./components/User";
+import { useSelector } from "react-redux";
+import Post from "./components/Post";
+
+const App = () => {
+  const posts = useSelector((state) => state.postReducer);
+  return (
+    <div>
+      <h1>Extreme</h1>
+      <PostForm />
+      <div className="content">
+        <div className="post-container">
+          {posts &&
+            posts.map((post, index) => <Post post={post} key={index} />)}
+        </div>
+        <User />
+      </div>
+    </div>
+  );
+};
+
+export default App;
