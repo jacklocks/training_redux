bdc02c534dcb433e53307c1c51aa236e8bcae30d
jacklocks <castets.eric@free.fr>
2023-11-06 08:43:23 +0100

    correction map

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import PostForm from "./components/PostForm";
 import User from "./components/User";
 import { useSelector } from "react-redux";
 import Post from "./components/Post";
+import { isEmpty } from "./components/Utils";
 
 const App = () => {
   const posts = useSelector((state) => state.postReducer);
@@ -12,7 +13,7 @@ const App = () => {
       <PostForm />
       <div className="content">
         <div className="post-container">
-          {posts &&
+          {!isEmpty(posts) &&
             posts.map((post, index) => <Post post={post} key={index} />)}
         </div>
         <User />
