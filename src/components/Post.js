8a94701728198096df8b1794ce24a2bfdef619e3
jacklocks <castets.eric@free.fr>
2023-11-08 16:38:06 +0100

    fin

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Like from "./Like";
 import { useDispatch, useSelector } from "react-redux";
 import { isEmpty } from "./Utils";
-import { editPost } from "../actions/post.action";
+import { deletePost, editPost } from "../actions/post.action";
 
 const Post = ({ post }) => {
   const [editToggle, setEditToggle] = useState(false);
@@ -34,7 +34,11 @@ const Post = ({ post }) => {
             alt="edit"
             onClick={() => setEditToggle(!editToggle)}
           />
-          <img src="./icons/delete.svg" alt="delete" />
+          <img
+            src="./icons/delete.svg"
+            alt="delete"
+            onClick={() => dispatch(deletePost(post.id))}
+          />
         </div>
       )}
 
